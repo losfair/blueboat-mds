08ec26166dd924ea4c164b660cfb172d59b5697d
losfair <zhy20000919@hotmail.com>
2021-11-29 07:44:20 +0000

    JS API fixes

--- mds/session.go
+++ mds/session.go
@@ -6,10 +6,8 @@ import (
 
 	"github.com/apple/foundationdb/bindings/go/src/fdb"
 	"github.com/apple/foundationdb/bindings/go/src/fdb/subspace"
-	"github.com/apple/foundationdb/bindings/go/src/fdb/tuple"
 	"github.com/dop251/goja"
 	"github.com/losfair/blueboat-mds/protocol"
-	"github.com/pkg/errors"
 	"go.uber.org/zap"
 	"google.golang.org/protobuf/proto"
 )
@@ -32,6 +30,7 @@ type jsReplicaTxn struct {
 }
 
 type jsFutureByteSlice struct {
+	s      *MdsSession
 	future fdb.FutureByteSlice
 }
 
@@ -39,18 +38,26 @@ type jsRangeResult struct {
 	rr fdb.RangeResult
 }
 
-func jsTxnCore_Get(s *MdsSession, txn fdb.ReadTransaction, key string) jsFutureByteSlice {
+type jsFutureNil struct {
+	future fdb.FutureNil
+}
+
+func jsTxnCore_Get(s *MdsSession, txn fdb.ReadTransaction, key goja.Value) jsFutureByteSlice {
+	rawKey := append([]byte(nil), s.ss.Bytes()...)
+	rawKey = append(rawKey, s.normalizeJsBytes(key)...)
 	return jsFutureByteSlice{
-		future: txn.Get(s.ss.Pack(tuple.Tuple{key})),
+		s:      s,
+		future: txn.Get(fdb.Key(rawKey)),
 	}
 }
 
-func jsTxnCore_PrefixList(s *MdsSession, txn fdb.ReadTransaction, prefix string, limit uint32) jsRangeResult {
+func jsTxnCore_PrefixList(s *MdsSession, txn fdb.ReadTransaction, prefix goja.Value, limit uint32) jsRangeResult {
 	if limit == 0 || limit > 1000 {
 		panic("PrefixList: invalid limit")
 	}
-
-	r, err := fdb.PrefixRange(s.ss.Pack(tuple.Tuple{prefix}))
+	rawKey := append([]byte(nil), s.ss.Bytes()...)
+	rawKey = append(rawKey, s.normalizeJsBytes(prefix)...)
+	r, err := fdb.PrefixRange(fdb.Key(rawKey))
 	if err != nil {
 		panic(err)
 	}
@@ -61,47 +68,60 @@ func jsTxnCore_PrefixList(s *MdsSession, txn fdb.ReadTransaction, prefix string,
 	}
 }
 
-func (t jsReplicaTxn) Get(key string) jsFutureByteSlice {
+func (t jsReplicaTxn) Get(key goja.Value) jsFutureByteSlice {
 	return jsTxnCore_Get(t.s, t.txn, key)
 }
 
-func (t jsReplicaTxn) PrefixList(prefix string, limit uint32) jsRangeResult {
+func (t jsReplicaTxn) PrefixList(prefix goja.Value, limit uint32) jsRangeResult {
 	return jsTxnCore_PrefixList(t.s, t.txn, prefix, limit)
 }
 
-func (f jsFutureByteSlice) Wait() []byte {
-	return f.future.MustGet()
+func (f jsFutureByteSlice) Wait() goja.ArrayBuffer {
+	buf := f.future.MustGet()
+	if buf == nil {
+		return goja.ArrayBuffer{}
+	} else {
+		return f.s.vm.NewArrayBuffer(buf)
+	}
 }
 
-func (t jsPrimaryTxn) Get(key string) jsFutureByteSlice {
+func (f jsFutureNil) Wait() {
+	f.future.MustGet()
+}
+
+func (t jsPrimaryTxn) Get(key goja.Value) jsFutureByteSlice {
 	return jsTxnCore_Get(t.s, t.txn, key)
 }
 
-func (t *jsPrimaryTxn) Set(key string, value []byte) {
-	t.txn.Set(t.s.ss.Pack(tuple.Tuple{key}), value)
+func (t jsPrimaryTxn) Set(key goja.Value, value goja.Value) goja.Value {
+	rawKey := append([]byte(nil), t.s.ss.Bytes()...)
+	rawKey = append(rawKey, t.s.normalizeJsBytes(key)...)
+	t.txn.Set(fdb.Key(rawKey), t.s.normalizeJsBytes(value))
+	return goja.Undefined()
 }
 
-func (t *jsPrimaryTxn) Delete(key string) {
-	t.txn.Clear(t.s.ss.Pack(tuple.Tuple{key}))
+func (t jsPrimaryTxn) Delete(key goja.Value) {
+	rawKey := append([]byte(nil), t.s.ss.Bytes()...)
+	rawKey = append(rawKey, t.s.normalizeJsBytes(key)...)
+	t.txn.Clear(fdb.Key(rawKey))
 }
 
-func (t jsPrimaryTxn) PrefixList(prefix string, limit uint32) jsRangeResult {
+func (t jsPrimaryTxn) PrefixList(prefix goja.Value, limit uint32) jsRangeResult {
 	return jsTxnCore_PrefixList(t.s, t.txn, prefix, limit)
 }
 
-func (t *jsPrimaryTxn) PrefixDelete(prefix string) {
-	r, err := fdb.PrefixRange(t.s.ss.Pack(tuple.Tuple{prefix}))
+func (t jsPrimaryTxn) PrefixDelete(prefix goja.Value) {
+	rawKey := append([]byte(nil), t.s.ss.Bytes()...)
+	rawKey = append(rawKey, t.s.normalizeJsBytes(prefix)...)
+	r, err := fdb.PrefixRange(fdb.Key(rawKey))
 	if err != nil {
 		panic(err)
 	}
 	t.txn.ClearRange(r)
 }
 
-func (t *jsPrimaryTxn) Commit() {
-	err := t.txn.Commit().Get()
-	if err != nil {
-		panic(errors.Wrap(err, "failed to commit transaction"))
-	}
+func (t jsPrimaryTxn) Commit() jsFutureNil {
+	return jsFutureNil{future: t.txn.Commit()}
 }
 
 func NewMdsSession(logger *zap.Logger, cluster *MdsCluster, ss subspace.Subspace) *MdsSession {
@@ -114,11 +134,41 @@ func NewMdsSession(logger *zap.Logger, cluster *MdsCluster, ss subspace.Subspace
 	s.vm.Set("createPrimaryTransaction", s.createPrimaryTransaction)
 	s.vm.Set("createReplicaTransaction", s.createReplicaTransaction)
 	s.vm.Set("base64Encode", s.jsBase64Encode)
+	s.vm.Set("base64Decode", s.jsBase64Decode)
+	s.vm.Set("stringToArrayBuffer", s.stringToArrayBuffer)
+	s.vm.Set("arrayBufferToString", s.arrayBufferToString)
 	return s
 }
 
 func (s *MdsSession) jsBase64Encode(value goja.ArrayBuffer) string {
 	return base64.StdEncoding.EncodeToString(value.Bytes())
+}
+
+func (s *MdsSession) jsBase64Decode(value string) goja.ArrayBuffer {
+	b, err := base64.StdEncoding.DecodeString(value)
+	if err != nil {
+		panic(err)
+	}
+	return s.vm.NewArrayBuffer(b)
+}
+
+func (s *MdsSession) stringToArrayBuffer(value string) goja.ArrayBuffer {
+	return s.vm.NewArrayBuffer([]byte(value))
+}
+
+func (s *MdsSession) arrayBufferToString(value goja.ArrayBuffer) string {
+	return string(value.Bytes())
+}
+
+func (s *MdsSession) normalizeJsBytes(value goja.Value) []byte {
+	exp := value.Export()
+	if v, ok := exp.(goja.ArrayBuffer); ok {
+		return v.Bytes()
+	} else if v, ok := exp.(string); ok {
+		return []byte(v)
+	} else {
+		panic("cannot normalize js value to bytes")
+	}
 }
 
 func (s *MdsSession) createPrimaryTransaction(call goja.FunctionCall) goja.Value {
