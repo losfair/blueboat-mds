6c9a57eee3ff5b43abaab270eda1f1928cb92b26
losfair <zhy20000919@hotmail.com>
2021-12-03 05:24:58 +0000

    Bump

--- mds/consts.go
+++ mds/consts.go
@@ -2,7 +2,7 @@ package mds
 
 import "time"
 
-const LoginVersion = "v0.1.0-alpha.2"
+const LoginVersion = "v0.1.0-alpha.3"
 const MaxWebSocketIncomingMessageSize = 1024 * 512 // 512KB
 const MaxMuxWidth = 16
 const PingInterval = 25 * time.Second
