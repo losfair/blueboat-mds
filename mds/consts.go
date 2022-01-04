01cb02a06c0db1c93ef292c2cd9022d2aeed861f
losfair <zhy20000919@hotmail.com>
2022-01-04 12:41:31 +0000

    Bump version

--- mds/consts.go
+++ mds/consts.go
@@ -2,7 +2,7 @@ package mds
 
 import "time"
 
-const LoginVersion = "v0.1.2-alpha.4"
+const LoginVersion = "v0.1.3-alpha.1"
 const MaxPreAuthWebSocketIncomingMessageSize = 1024 * 100 // 100KiB
 const MaxWebSocketIncomingMessageSize = 1024 * 1024 * 20  // 20MiB
 const MaxMuxWidth = 64
